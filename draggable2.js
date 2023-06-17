8fcb92f9c248fdd98fff744e5c5b19f82de2c6f7
lupoworld <135106765+lupoworld@users.noreply.github.com>
2023-06-16 23:09:42 -0700

    Update draggable2.js

--- draggable2.js
+++ draggable2.js
@@ -15,7 +15,7 @@
   }
 });
 */
-$(function () {
+/*$(function () {
 
 cardHover(); //intially call hover function
 
@@ -32,8 +32,22 @@ function cardHover() {
 
   };
 });
+*/
+$(function () {
+const nodeList = document.querySelectorAll(".gallery_card");
+for (let i = 0; i < nodeList.length; i++) {
+  nodeList[i].style.transform = "scale(1,1)";
+  nodeList[i].onmouseout = function(){
+        nodeList[i].style.transform = "scale(1,1)";
+  };
+    nodeList[i].onmouseenter = function(){
 
+    nodeList[i].style.transform = "scale(2,2)";
+  };
 
+  };
+};
+});
 $(".bubble").hover(function() {
   document.getElementsByClassName("bubble")[0].style.transform += "scale(1.1)";
 }, function() {
