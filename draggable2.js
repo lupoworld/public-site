96a4483613e6708d720ac04c17a23af62f2bfcb7
lupoworld <135106765+lupoworld@users.noreply.github.com>
2023-06-16 17:16:14 -0700

    Update draggable2.js

--- draggable2.js
+++ draggable2.js
@@ -1,6 +1,4 @@
-$(function(){
-  if($('div').is('.Index')){
-  $(".bubble").hover(function() {
+ $(".bubble").hover(function() {
   document.getElementsByClassName("bubble")[0].style.transform += "scale(1.1)";
 }, function() {
   if (document.getElementsByClassName("bubble")[0].style.transform[0] != "s") {
@@ -9,6 +7,14 @@ $(function(){
     document.getElementsByClassName("bubble")[0].style.transform = "";
   }
 });
+
+
+ $(".gallery_card").hover(function() {
+  document.getElementsByClassName("gallery_card")[0].style.transform += "scale(1.1)";
+}, function() {
+  if (document.getElementsByClassName("gallery_card")[0].style.transform[0] != "s") {
+    document.getElementsByClassName("gallery_card")[0].style.transform = document.getElementsByClassName("bubble")[0].style.transform.split(")")[0];
+  } else {
+    document.getElementsByClassName("gallery_card")[0].style.transform = "";
+  }
 });
-
-
