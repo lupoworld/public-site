8280f9c8590c55dd524b77a56b25de095f081e2a
lupoworld <135106765+lupoworld@users.noreply.github.com>
2023-06-17 01:04:08 -0700

    Update draggable2.js

--- draggable2.js
+++ draggable2.js
@@ -1,4 +1,4 @@
-let cardList = document.querySelectorAll(".gallery_card_image");
+let cardList = document.querySelectorAll("#bean");
 console.log(cardList);
 for (let i = 0; i < cardList.length; i++) {
   cardList[i].style.transform = "scale(1,1)";
